a8bcd198ca9d8e2fcce22cf30211561759cebbcb
ingr1dayala <115100600+ingr1dayala@users.noreply.github.com>
2022-10-26 11:22:45 -0500

    Add files via upload

---
+++ holamundo.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+<TITLE>Top HTML Tags</TITLE>
+</HEAD>
+<BODY>
+HTML is swell.<BR>
+Life is good.
+  <?php
+  echo "hello world";
+  ?>
+<H3>Here's the big picture</H3>
+<IMG src="https://media3.giphy.com/media/zzALYeLqMLDa6PEV2C/giphy.gif">
+<UL>Make me an unordered list.
+<LI>One programmer</LI>
+<LI>Ten SDKs</LI>
+<LI>Great Internet Apps</LI>
+</UL>
+</BODY>
+</HTML>
